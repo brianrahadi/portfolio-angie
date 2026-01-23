4b77f0593d5e3a21a30c48e58ff33538a18af674
Angelique Clara Hanzell <97010202+angieclra@users.noreply.github.com>
2026-01-22 20:34:22 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    base: "/portfolio/"
     port: 8080,
     hmr: {
       overlay: false,
